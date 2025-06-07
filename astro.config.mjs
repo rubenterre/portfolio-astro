9b44f2e609dab4ba45f9ef6e78dde4bc3ed6371c
Rubén Terré <72130385+rubenterre@users.noreply.github.com>
2025-06-07 08:46:20 +0200

    Update: add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import svelte from '@astrojs/svelte';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: [svelte()],
+  adapter: netlify()
 });
