25f9e00d6fc45ebaaaa59c881522dafd2f445132
Rubén Terré <72130385+rubenterre@users.noreply.github.com>
2025-06-08 10:41:14 +0200

    Update index.astro

---
+++ .netlify/build/entry.mjs
@@ -0,0 +1,30 @@
+import { r as renderers } from './chunks/_@astro-renderers_DNXR2CeT.mjs';
+import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CvSoi7hX.mjs';
+import { manifest } from './manifest_AnNv8MR4.mjs';
+import { createExports } from '@astrojs/netlify/ssr-function.js';
+
+const serverIslandMap = new Map();;
+
+const _page0 = () => import('./pages/index.astro.mjs');
+const pageMap = new Map([
+    ["src/pages/index.astro", _page0]
+]);
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    actions: () => import('./_noop-actions.mjs'),
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "3b73ff89-767c-4b49-ad95-a75b4c0b310a"
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+const _start = 'start';
+if (_start in serverEntrypointModule) {
+	serverEntrypointModule[_start](_manifest, _args);
+}
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
